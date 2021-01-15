65e58589cbcd6d714307528f664c0295239f98bb
Mohsin Azam <mohsin.azam@systemsltd.com>
2021-01-15 17:17:44 +0500

    Add project files.

---
+++ Scope-Transient/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+namespace Scope_Transient
+{
+
+    public class Scoped
+    {
+    }
+    public class Transient
+    {
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var collection = new ServiceCollection();
+            collection.AddScoped<Scoped>();
+            collection.AddTransient<Transient>();
+
+            var builder = collection.BuildServiceProvider();
+
+            Console.Clear();
+            Parallel.For(1, 5, i =>
+            {
+                Console.WriteLine($"Scope ID =  { builder.GetService<Scoped>().GetHashCode().ToString()}");
+                Console.WriteLine($"Transient ID =  { builder.GetService<Transient>().GetHashCode().ToString()}");
+            });
+
+            Console.WriteLine("Press any key...");
+            Console.ReadKey();
+        }
+    }
+}
